f9fe9c6c470bb384fa5b6fefebf2e8d30b4b6f45
Diana <maritimdiane96@gmail.com>
2022-11-30 16:53:26 +0300

    addition to file

--- grade.js
+++ grade.js
@@ -1,8 +1,8 @@
-// prompt command,initiate using npm install propt sync
+// prompt command,initiate using npm install prompt-sync
 const prompt = require("prompt-sync")();
 let grade = prompt('Enter grade of student:');
 {
-//student grade generation conditions
+//student grade generation conditions using if, else if and else statements
 if (grade <= 100 && grade >79){
     console.log('A')
 }
@@ -10,7 +10,6 @@ if (grade <= 100 && grade >79){
 else if (grade <= 79 && grade >= 60 ){
     console.log ('B')
 }
-
 else if (grade <= 59 && grade > 49){
     console.log('C')
 }
